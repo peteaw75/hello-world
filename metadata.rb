c24b9de5a47100af7c9ece335ff104c1d01af795
peteaw75 <peter.aw@outlook.com>
2016-08-18 15:00:26 +0800

    Add files via upload

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'test1_centos'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures test1_centos'
+long_description 'Installs/Configures test1_centos'
+version '0.1.0'
+
+depends 'selinux', '~> 0.9.0'
